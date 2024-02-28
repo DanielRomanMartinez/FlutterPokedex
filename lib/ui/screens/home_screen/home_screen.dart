c4ef92c590c4b1c11fa28c7e844581146e0f5aa8
Dani Roman <daniel.roman@thehiddenpanda.com>
2024-02-28 16:42:03 +0100

    Custom bottom navigation bar

--- lib/ui/screens/home_screen/home_screen.dart
+++ lib/ui/screens/home_screen/home_screen.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_pokedex/application/bloc/custom_bottom_navigation/custom_bottom_navigation_bloc.dart';
 import 'package:flutter_pokedex/ui/common/widgets/application_layout/application_layout.dart';
-import 'package:flutter_pokedex/ui/common/widgets/custom_bottom_navigation_bar/custom_bottom_navigation_bar.dart';
-import 'package:get_it/get_it.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = 'home';
-
-  final CustomBottomNavigationBloc _customBottomNavigationBloc = GetIt.instance.get<CustomBottomNavigationBloc>();
-
-   HomeScreen({
+   const HomeScreen({
     super.key,
   });
 
