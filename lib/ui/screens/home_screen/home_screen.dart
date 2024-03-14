46f7838ab930a75526dd8949ba23d0de4f80b972
Dani Roman <daniel.roman@thehiddenpanda.com>
2024-03-14 01:28:34 +0100

    Detect if user has connection or not

--- lib/ui/screens/home_screen/home_screen.dart
+++ lib/ui/screens/home_screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_widget/connectivity_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_pokedex/application/bloc/home_screen/home_screen_bloc.dart';
@@ -8,6 +9,8 @@ import 'package:flutter_pokedex/ui/common/widgets/pokemon_card/pokemon_card.dart
 import 'package:flutter_pokedex/ui/screens/pokemon_detail_screen/pokemon_detail_screen.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
+
+part 'widgets/error_message.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = 'home';
@@ -32,74 +35,56 @@ class HomeScreen extends StatelessWidget {
           child: BlocBuilder<HomeScreenBloc, HomeScreenState>(
             bloc: _homeScreenBloc,
             builder: (context, state) {
-              if (state is! HomeScreenLoaded && state is! HomeScreenError) {
-                _homeScreenBloc.add(const LoadHomeScreen());
-              }
+              return ConnectivityWidget(
+                showOfflineBanner: false,
+                builder: (context, isOnline) {
 
-              if (state is HomeScreenLoaded) {
-                return ListView.builder(
-                  itemCount: state.information['content'].length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Column(
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            context
-                                .push("/${PokemonDetailScreen.routeName}/${state.information['content'][index].name}");
-                          },
-                          child: PokemonCard(state.information['content'][index]),
-                        ),
-                        const SizedBox(height: Shapes.gutter),
-                      ],
-                    );
-                  },
-                );
-              }
-
-              if (state is HomeScreenError) {
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Something went wrong',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      const SizedBox(height: Shapes.gutter2x),
-                      InkWell(
-                        onTap: () => _homeScreenBloc.add(const LoadHomeScreen()),
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 10,
-                            horizontal: 10,
-                          ),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            color: const Color(0xFF173EA5),
-                          ),
-                          child: const Text(
-                            'Try again',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                              fontWeight: FontWeight.w600,
+                  if (state is HomeScreenLoaded) {
+                    return ListView.builder(
+                      itemCount: state.information['content'].length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Column(
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                context.push(
+                                    "/${PokemonDetailScreen.routeName}/${state.information['content'][index].name}");
+                              },
+                              child: PokemonCard(state.information['content'][index]),
                             ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                );
-              }
+                            const SizedBox(height: Shapes.gutter),
+                          ],
+                        );
+                      },
+                    );
+                  }
 
-              return const Center(
-                child: CircularProgressIndicator(
-                  color: User.defaultBackgroundColor,
-                ),
+                  if (isOnline) {
+                    if (state is! HomeScreenLoaded && state is! HomeScreenError) {
+                      _homeScreenBloc.add(const LoadHomeScreen());
+                    }
+
+                    if (state is HomeScreenError) {
+                      return ErrorMessage(
+                        text: 'Something went wrong',
+                        onRetry: () => _homeScreenBloc.add(const LoadHomeScreen()),
+                      );
+                    }
+
+                    return const Center(
+                      child: CircularProgressIndicator(
+                        color: User.defaultBackgroundColor,
+                      ),
+                    );
+                  }
+
+
+
+                  return ErrorMessage(
+                    text: 'No connectivity',
+                    onRetry: () => _homeScreenBloc.add(const LoadHomeScreen()),
+                  );
+                },
               );
             },
           ),
